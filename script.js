c0cecb2bbf1005fa457e455ff470d83e37b771cd
Mahrin03 <mahinshoily8@gmail.com>
2026-07-04 21:12:45 +0600

    update 7

---
+++ script.js
@@ -0,0 +1,23 @@
+function showOffer() {
+    alert("Today only: Get 10% discount on all GreenBite combo packs!");
+}
+
+function changeHeroText() {
+    var message = document.getElementById("homeMessage");
+    message.innerHTML = "GreenBite is a good choice because our snacks are simple, fresh, affordable and easy to order online.";
+}
+
+function addToCart(productName) {
+    alert(productName + " has been added to your cart.");
+}
+
+function sendMessage() {
+    var name = document.getElementById("name").value;
+    var result = document.getElementById("contactResult");
+
+    if (name == "") {
+        result.innerHTML = "Please enter your name before submitting.";
+    } else {
+        result.innerHTML = "Thank you, " + name + ". Your message has been received.";
+    }
+}
